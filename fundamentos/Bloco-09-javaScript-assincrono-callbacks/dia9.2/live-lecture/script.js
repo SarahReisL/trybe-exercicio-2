310838f85af31088c154df816112462381c61be4
Sarah Reis <sahreis16@gmail.com>
2022-03-22 18:03:30 -0300

    Add bloco 9.2

--- fundamentos/Bloco-09-javaScript-assincrono-callbacks/dia9.2/live-lecture/script.js
+++ fundamentos/Bloco-09-javaScript-assincrono-callbacks/dia9.2/live-lecture/script.js
@@ -1,7 +1,7 @@
-function append(data) {
+function append(data) { // function de anexar
     const ul = document.querySelector('ul');
     
-    const li = document.createElement('li');
+    const li = document.createElement('li'); // criando item pra ser add na lista
     const divNome = document.createElement('div');
     const divImage = document.createElement('div');
     const img = document.createElement('img');
@@ -18,18 +18,100 @@ function append(data) {
 
   // 1. Implemente a função fetchPokemon que vai buscar os dados dos Pokémons na API
 
+  function fetchPokemon() {
+    const promise = fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    console.log(promise);
+    promise.then((response) => {
+     const promiseJSON = response.json();
+     promiseJSON.then((data) => {
+       console.log(data);
+       const pokemon = {
+         name: data.name,
+         imageUrl: data.sprites.front_default
+       }
+       append(pokemon)
+     })
+    })
+  }
  
   // 2. Refatore a função fetchPokemon para utilizar encadeamento
 
-
+  function fetchPokemon(poke) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
+    .then((response) => {
+      response.json()
+     .then((data) => {
+       const pokemon = {
+         name: data.name,
+         imageUrl: data.sprites.front_default
+       }
+       append(pokemon)
+     })
+    })
+  }
   // 3. Crie uma função requestPokemons que vai chamar a função fetchPokemon para adquirir alguns Pokémons
 
-
+function requestPokemons() {
+  fetchPokemon("pikachu");
+  fetchPokemon("charmander");
+  fetchPokemon("squirtle");
+  fetchPokemon("lapras");
+  fetchPokemon("abra");
+}
   //4. Adicione o tratamento de erro para o fetch
 
-  // 5. Refatorar a função fetchPokemon criando a função extractNameAndImage
+  function fetchPokemon(poke) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
+      .then((response) => {
+        response.json()
+          .then((data) => {
+            const pokemon = {
+              name: data.name,
+              imageUrl: data.sprites.front_default
+            }    
+        append(pokemon)
+      })
+      .catch ((error) => console.log(error))
+    })
+  }
 
+  // 5. Refatorar a função fetchPokemon criando a função extractNameAndImage
+  function extractNameAndImage(data) {
+    const pokemon = {
+      name: data.name,
+      imageUrl: data.sprites.front_default
+    }
+    return pokemon;
+  }
+
+  function fetchPokemons(poke) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
+      .then((response) => {
+        response.json()
+          .then((data) => {
+            const pokemon = extractNameAndImage(data)   
+            append(pokemon)
+          })
+      .catch ((error) => console.log(error))
+    })
+  }
 
   //6. Criar uma função que utilize o async/await para fazer as requisições 
   
-  
+  // async function fetchPokemon(poke) {
+
+  // }
+
+  async function fetchPokemon(poke) {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
+      console.log(response)
+      const data = await response.json();
+      const pokemon = extractNameAndImage(data);
+      append(pokemon)
+      } catch (error) {
+      console.log(error)
+    }
+  }
+
+  window.onload = requestPokemons;
